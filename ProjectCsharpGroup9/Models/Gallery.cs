cd4baf658f6657c48b85221fa2a883ae65f696ee
nihongoo <127075883+nihongoo@users.noreply.github.com>
2024-05-17 12:17:44 +0700

    -Hảo_Done class -Đã cài Nuget mn không cần cài lại đâu -Nuget dùng bản 7.0.19

---
+++ ProjectCsharpGroup9/Models/Gallery.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectCsharpGroup9.Models
+{
+    public class Gallery
+    {
+        public Guid GalleryID { get; set; }
+        public Guid ProductID { get; set; }
+        public string ImagePath { get; set; }
+        public string Description { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
---
+++ ProjectCsharpGroup9/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace ProjectCsharpGroup9.Models
+{
+    public class Product
+    {
+        public Guid ProductID { get; set; }
+        public string ProductName { get; set; }
+        public DateTime InputDay { get; set; }
+        public Guid CategoryId { get; set; }
+        public decimal RegularPrice { get; set; }
+        public decimal DiscountPrice { get; set; }
+        public Guid BrandID { get; set; }
+        public int Quantity { get; set; }
+        public double ProductWeight { get; set; }
+        public string Description { get; set; }
+        public virtual List<Gallery> Galleries { get; set; }
+        public virtual List<CartDetail> CartDetails { get; set; }
+        public virtual List<BillDetail> BillDetails { get; set; }
+        public virtual Category Category { get; set; }
+        public virtual Brand Brand { get; set; }
+    }
+}
