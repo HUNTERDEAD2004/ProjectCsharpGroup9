b18ab286e868a5741a53c99275af05751ff9ab29
nihongoo <127075883+nihongoo@users.noreply.github.com>
2024-06-11 17:39:57 +0700

    CRUD product web MVC

--- ProjectCsharpGroup9/Areas/Admin/Controllers/ProductController.cs
+++ ProjectCsharpGroup9/Areas/Admin/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using ProjectCsharpGroup9.Models;
 using System;
 using System.Collections.Generic;
@@ -13,15 +14,59 @@ namespace ProjectCsharpGroup9.Areas.Admin.Controllers
     [Route("Admin/Product")]
     public class ProductController : Controller
     {
-        private readonly string _apiBaseUrl = "https://localhost:7276/api/Product";
-        HttpClient _httpClient;
-        
-        public ProductController()
+        HttpClient _client = new HttpClient();
+        [HttpGet("Index")]
+        public ActionResult Index()
         {
-            _httpClient = new HttpClient();
+            string Url = $@"https://localhost:7276/api/Product/Get-All-Product";
+            var response = _client.GetStringAsync(Url).Result;
+            List<Product> products = JsonConvert.DeserializeObject<List<Product>>(response);
+            return View(products);
         }
-        
+        [HttpGet("Detail/{id}")]
+        public ActionResult Details(Guid id)
+        {
+            string Url = $@"https://localhost:7276/api/Product/Get-ID-Product?id={id}";
+            var response = _client.GetStringAsync(Url).Result;
+            Product products = JsonConvert.DeserializeObject<Product>(response);
+            return View(products);
+        }
+        [HttpGet("Create")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost("Create")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product product)
+        {
+            string Url = $@"https://localhost:7276/api/Product/Create-Product";
+            var response = _client.PostAsJsonAsync(Url, product).Result;
+            return RedirectToAction("Index");
+        }
+        [HttpGet("Edit/{id}")]
+        public ActionResult Edit(Guid id)
+        {
+            string Url = $@"https://localhost:7276/api/Product/Get-ID-Product?id={id}";
+            var response = _client.GetStringAsync(Url).Result;
+            Product products = JsonConvert.DeserializeObject<Product>(response);
+            return View(products);
+        }
+        [HttpPost("Edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product product)
+        {
+            string Url = $@"https://localhost:7276/api/Product/Edit-Product";
+            var response = _client.PutAsJsonAsync(Url, product).Result;
+            return RedirectToAction("Index");
+        }
+        [Route("Delete/{id}")]
+        public ActionResult Delete(Guid id)
+        {
+            string Url = $@"https://localhost:7276/api/Product/Delete-Product?id={id}";
+            var response = _client.DeleteAsync(Url).Result;
+            return RedirectToAction("Index");
+        }
 
-        
     }
 }
